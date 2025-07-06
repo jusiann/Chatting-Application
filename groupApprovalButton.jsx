5a44e331717d3bb9184262a8ed9ee2237b236d23
nurdk43 <dikaysenur4335@gmail.com>
2025-07-07 02:39:48 +0300

    Add files via upload

---
+++ groupApprovalButton.jsx
@@ -0,0 +1,13 @@
+import "../style/groupApprovalButton.css";
+import { CheckCircle } from "lucide-react";
+
+const GroupApprovalButton = () => {
+    return (
+        <div className="groupApprovalButton">
+            <span>Onayla</span>
+            <CheckCircle />
+        </div>
+    );
+};
+
+export default GroupApprovalButton;
