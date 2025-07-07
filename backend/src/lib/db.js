c7b45fa29c07bba9ac3b519edb4c5fd40419a184
Metehan Buçan <metehanbucan@gmail.com>
2025-07-07 17:09:00 +0300

    Cleaning bad Commits

---
+++ backend/src/lib/db.js
@@ -0,0 +1,17 @@
+
+import {Client} from "pg";
+
+const client = new Client({
+    user: "postgres",
+    host: "localhost",
+    database:"chat_app",
+    password:"chatapp123",
+    port: "5432",
+});
+
+client.connect()
+    .then(() => console.log("Successfully connected to Database"))
+    .catch(err => console.error("Database connection failed", err));
+
+export default client;
+
